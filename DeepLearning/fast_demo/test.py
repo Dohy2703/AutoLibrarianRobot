cc09e6f7a10d1ce4bed137b4d5afbc5e04997823
Dohy2703 <125836071+Dohy2703@users.noreply.github.com>
2023-09-02 20:30:53 +0900

    Update test.py

--- DeepLearning/fast_demo/test.py
+++ DeepLearning/fast_demo/test.py
@@ -9,9 +9,9 @@ import os, sys
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 from config.fast.tt import fast_base_tt_512_finetune_ic17mlt
 
-cfg = fast_base_tt_512_finetune_ic17mlt
+def setup_fast():
+    cfg = fast_base_tt_512_finetune_ic17mlt
 
-if __name__ == '__main__':
     model = FAST()
     model = model.cuda()
     checkpoint = torch.load('model/weights.pth')
@@ -26,9 +26,13 @@ if __name__ == '__main__':
     model = fuse_module(model)
     model.eval()
 
-    start_t = time.time()
+    return model, cfg
 
-    image_path = './sample.png'
+def crop_imgs(image_path, model, cfg, visualize=False, count_time=True):
+    if count_time:
+        start_t = time.time()
+
+    # image_path = './sample.png'
     img = cv2.imread(image_path)
     image = process_image(image_path)
 
@@ -38,17 +42,60 @@ if __name__ == '__main__':
         outputs = model(**image, cfg=cfg)
         result = outputs['results'][0]['bboxes']
 
+    crop_img = []
+
     for bbox in result:
         points = np.array(bbox).reshape(-1,2)
-        # for pt in points:
-        #     cv2.circle(img, pt, 1, (0, 255, 0), 2)
-        rect = cv2.minAreaRect(points)
-        box = cv2.boxPoints(rect)
-        box = np.intp(box)
-        cv2.drawContours(img, [box], 0, (0,255,0), 3)
 
-    end_t = time.time()
-    print('reference time : ', end_t - start_t)
+        ''' visualize points '''
+        # if visualize:
+        #     for pt in points:
+        #         cv2.circle(img, pt, 1, (0, 255, 0), 2)
+        ''' visualize minarearect '''
+        # if visualize:
+        #     rect = cv2.minAreaRect(points)
+        #     box = cv2.boxPoints(rect)
+        #     box = np.intp(box)
+        #     cv2.drawContours(img, [box], 0, (0,255,0), 3)
+        ''' visualize bbox '''
+        x, y, w, h = cv2.boundingRect(points)
+        if visualize:
+            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 3)
 
-    cv2.imshow('img', img)
-    cv2.waitKey(0)
+        crop_img.append(img[y:y + h, x:x + w])
+        if visualize:
+            cv2.imshow('crop_img', img[y:y + h, x:x + w])
+            cv2.waitKey(0)
+
+    if count_time:
+        end_t = time.time()
+        print('reference time : ', end_t - start_t)
+
+    if visualize:
+        cv2.imshow('img', img)
+        cv2.waitKey(0)
+
+    return crop_img
+
+
+def read_text_easyocr(crop_img):
+    try:
+        import EasyOCR.easyocr.easyocr as easyocr
+    except:
+        import easyocr  # pip install easyocr
+
+    reader = easyocr.Reader(['en'])
+
+    for item in crop_img:
+        result = reader.recognize(item)
+        for i in range(len(result)):
+            print(result[i][1])
+
+    # reader = easyocr.Reader(['en'])  # 한글도 적용하고 싶으면 ['ko', 'en']. 다만 여기선 자음만 인식 안돼서 안씀
+    # result = reader.readtext(sharpening_img, slope_ths=0.3)
+
+
+if __name__ == '__main__':
+    model, cfg = setup_fast()
+    fast_img = crop_imgs(image_path='./sample.png', model=model, cfg=cfg, visualize=False)
+    # read_text_easyocr(fast_img)
